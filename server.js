f67dd88d8ce282162ecf3628e0adf916475e5a15
ZachWagner1 <zwag14@yahoo.com>
2020-07-30 21:00:23 -0500

    full development

--- server.js
+++ server.js
@@ -1,35 +1,22 @@
+const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const express = require('express');
-const { animals } = require('./data/animals.json');
-
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
-// parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
-// provide css to html
+// if client navigates to '<host>/api', use router set up in apiRoutes
+app.use('/api', apiRoutes);
+// if client navigates to '<host>/', use router set up in htmlRoutes
+app.use('/', htmlRoutes);
+// middleware to make static assets readily available, not gated behind a server endpoint
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.get('/animals', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/animals.html'));
-});
-
-app.get('/zookeepers', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
-});
-
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+	console.log(`API server now on port ${PORT}!`);
 });
-
-// Next, we'll need to change all of the app routes to use router instead. 
-// In every routing block in the code, change app to route. For example, 
-// the GET route will look like this: 11.4.4
